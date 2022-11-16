9fa7a8a8c6315f589a546003333664dc686165b1
Adeyemo Adeolu <adex9ja@yahoo.com>
2022-11-16 18:06:30 +0000

    fix test

--- src/pairtest/TicketService.js
+++ src/pairtest/TicketService.js
@@ -2,37 +2,35 @@ import TicketTypeRequest from './lib/TicketTypeRequest.js';
 import InvalidPurchaseException from './lib/InvalidPurchaseException.js';
 import TicketPaymentService from '../thirdparty/paymentgateway/TicketPaymentService.js';
 import SeatReservationService from '../thirdparty/seatbooking/SeatReservationService.js';
-
 export default class TicketService {
   /**
    * Should only have private methods other than the one below.
    */
 
   purchaseTickets(accountId, ...ticketTypeRequests) {
-    // Return value
     let returnValue = -1;
 
     if(accountId <= 0){
-      throw new InvalidPurchaseException("Invalid accountId", `Invalid account Id: ${accountId}`);
+      throw new InvalidPurchaseException(`Invalid account Id: ${accountId}`);
     }
 
-    // Check if the number of the ticket types bought is already over the limit, then there will be no need to check how many tickets bought in total
-    if(ticketTypeRequests.length <= 0){
-      throw new InvalidPurchaseException("Tickets cannot be less than 1", "Minimum number of tickets not bought");
-    }else if(ticketTypeRequests.length > 20){
-      throw new InvalidPurchaseException("Maximum number of tickets exceeded", `Maximum number of tickets exceeded by: ${ticketTypeRequests.length - 20}`);
-    }else{
-      let ticketType = this.#getNumSeatsAmount(...ticketTypeRequests);
+    if(ticketTypeRequests.length <= 0)
+      throw new InvalidPurchaseException("Tickets cannot be less than 1");
+    if(ticketTypeRequests.length > 20)
+      throw new InvalidPurchaseException("Maximum number of tickets exceeded");
 
-      // Make payment
-      new TicketPaymentService().makePayment(accountId, ticketType.totalAmount);
 
-      // Reserve seats
-      new SeatReservationService().reserveSeat(accountId, ticketType.noAdult + ticketType.noChild)
+    let ticketType = this.#getNumSeatsAmount(...ticketTypeRequests);
 
-      // Return total amount and number of seats, if everything works smoothly
-      returnValue = [ticketType.totalAmount, ticketType.noAdult + ticketType.noChild];
-    }
+    // Make payment
+    new TicketPaymentService().makePayment(accountId, ticketType.totalAmount);
+
+    // Reserve seats
+    new SeatReservationService().reserveSeat(accountId, ticketType.noAdult + ticketType.noChild)
+
+    // Return total amount and number of seats, if everything works smoothly
+    returnValue = [ticketType.totalAmount, ticketType.noAdult + ticketType.noChild];
+
 
     return returnValue;
   }
@@ -48,7 +46,7 @@ export default class TicketService {
       child : 10,
       adult : 20
     }
-    ticketTypeRequests.forEach(ticket => {
+    ticketTypeRequests.forEach((ticket) => {
       switch(ticket.getTicketType()){
         case "INFANT":
           noInfant += ticket.getNoOfTickets();
@@ -60,7 +58,7 @@ export default class TicketService {
           noAdult += ticket.getNoOfTickets();
           break;
         default:
-          throw new InvalidPurchaseException("Invalid ticket type", `Invalid ticket type: ${ticket.getTicketType()}`);
+          throw new InvalidPurchaseException(`Invalid ticket type: ${ticket.getTicketType()}`);
       }
     });
 
@@ -69,7 +67,7 @@ export default class TicketService {
 
     // Assumption: Only one Adult or Child is permitted to hold one Infant
     if(noAdult + noChild < noInfant){
-      throw new InvalidPurchaseException("Cannot have more infants", `Invalid number of infants: ${noInfant}`);
+      throw new InvalidPurchaseException(`Invalid number of infants: ${noInfant}`);
     }
 
     return {
--- test/TicketService.test.js
+++ test/TicketService.test.js
@@ -1,25 +1,25 @@
 import TicketTypeRequest from "../src/pairtest/lib/TicketTypeRequest";
 import TicketService from "../src/pairtest/TicketService";
 
-describe("TicketService methods testing", () => {
+describe("TicketService", () => {
     let ticketService = new TicketService();
     let adults = new TicketTypeRequest("ADULT", 3);
     let infants = new TicketTypeRequest("INFANT", 5);
     let children = new TicketTypeRequest("CHILD", 5);
 
     test("Number of adults and children more than number of infants", () => {
-        expect(ticketService.purchaseTickets(5, [adults, infants, children])).toEqual([110, 8]);
+        expect(ticketService.purchaseTickets(1, [adults, infants, children])).toEqual([110, 8]);
     });
 
     //
     test("Number of adults and children equal to number of infants", () => {
         let infants = new TicketTypeRequest("INFANT", 8);
-        expect(ticketService.purchaseTickets(5, [adults, infants, children])).toEqual([110, 8]);
+        expect(ticketService.purchaseTickets(2, [adults, infants, children])).toEqual([110, 8]);
     });
 
     //
     test("Number of children more than number of infants and adults", () => {
         let children = new TicketTypeRequest("CHILD", 10);
-        expect(ticketService.purchaseTickets(5, [adults, infants, children])).toEqual([160, 13]);
+        expect(ticketService.purchaseTickets(3, [adults, infants, children])).toEqual([160, 13]);
     });
 });
